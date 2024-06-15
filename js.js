d32ff80b5051794b6da9715d5caab3ce6875c22d
Guilherme Fernandes de Aguiar <guiilherrme.aguiiar@gmail.com>
2024-06-14 21:01:08 -0300

    start my project

---
+++ js.js
@@ -0,0 +1,13 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    const ul = document.querySelector('ul');
+    ul.classList.toggle('active');
+
+
+
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+console.log('teste');
